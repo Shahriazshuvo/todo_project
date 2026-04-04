d759fd6f6afaf4f24e4536399e3017cb1a8a04ab
Montakim <Shahriaz.Montakim@brainstation-23.com>
2026-04-04 11:19:29 +0600

    set up model and register it admin

--- todo_app/task/admin.py
+++ todo_app/task/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from todo_app.task.models import Task
 
 # Register your models here.
+
+admin.site.register(Task)
--- todo_app/task/models.py
+++ todo_app/task/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks",null=True)
+	title = models.CharField(max_length=255)
+	description = models.TextField(blank=True,null=True)
+	completed = models.BooleanField(default=False)
+	due_date = models.DateTimeField(null=True, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ["-created_at"]
+
+	def __str__(self):
+		return self.title
